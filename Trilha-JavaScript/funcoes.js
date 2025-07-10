24af952de337b9a07997ece001d6d19e93303fb1
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-07-10 11:24:49 -0300

    add funcoes.js with functions for printing text and summing numbers

---
+++ Trilha-JavaScript/funcoes.js
@@ -0,0 +1,11 @@
+function imprimeTexto(texto){
+    console.log(texto)
+}
+
+const ola = imprimeTexto("Olá Mundoo")
+
+function soma(numero1, numero2){
+    return console.log(numero1 + numero2)
+}
+
+const conta = soma(2,5)
