8718bcaff180e95fff30e3b17f31df7285218165
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-07-10 10:39:30 -0300

    add template-strings.js with example of template literals for string interpolation

---
+++ Trilha-JavaScript/template-strings.js
@@ -0,0 +1,5 @@
+const nome = "Kaiky"
+const idade = 21
+const cidadeDeNascimento = "Ouro Branco/MG"
+const apresentacao = `Meu nome é ${nome} tenho ${idade} anos e nasci em ${cidadeDeNascimento}`
+console.log(apresentacao)
