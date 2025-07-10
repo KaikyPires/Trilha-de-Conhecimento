e43e6bb4deedd5e91be5d07f8c294ad90a1fb666
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-07-10 09:10:04 -0300

    add typeNumber.js with examples of number operations and NaN handling

---
+++ Trilha-JavaScript/typeNumber.js
@@ -0,0 +1,24 @@
+// tipo number
+
+const meuNumero = 3
+
+let seuNumero = 1
+
+const operacaoMatematica = meuNumero + seuNumero
+
+console.log('o resultado foi: ',operacaoMatematica)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3
+const pontoFluenteSemZero = .5
+
+const novaOperacao = meuNumero / pontoFluenteSemZero
+
+console.log('resultado:', novaOperacao)
+
+// NaN -> Not a Number 
+
+const alura = 'Alura'
+
+console.log(alura * meuNumero)
