37070e101ce647dca3dab40253bb7efd3d35c24b
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-07-10 09:09:56 -0300

    add initial implementation of typeString.js with string variables and concatenation example

---
+++ Trilha-JavaScript/typeString.js
@@ -0,0 +1,10 @@
+const texto1 = 'Ola mundo'
+const senha = 'senhaSegura123'
+
+const citacao = 'O leo disse "oi"'
+
+const citacao2 = 'Meu nome é '
+const meuNome = 'Kaiky'
+
+// concatenação (+)
+console.log(citacao2 + meuNome)
