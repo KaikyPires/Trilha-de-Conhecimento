d9ad29755ae6992c49d07e243333100a843f5407
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-07-11 11:37:33 -0300

    refactor aplicarDesconto to ensure prices are formatted to two decimal places

--- alura_books-projeto_inicial/app/metodoMap.js
+++ alura_books-projeto_inicial/app/metodoMap.js
@@ -1,7 +1,9 @@
 function aplicarDesconto(livros){
     const desconto = 0.3
-    livrosComDesconto = livros.map(livro =>{
-        return {...livro, preco:livro.preco - (livro.preco * desconto)}
+    const livrosComDesconto = livros.map(livro =>{
+        return {...livro, 
+            preco: Number((livro.preco - (livro.preco * desconto)).toFixed(2))
+        }
     })
     return livrosComDesconto
 }
